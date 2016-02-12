458aae02dab39bd2fefb27e2fff9db64cae75b90
ahmedalaasoliman <ahmed.alaasoliman@gmail.com>
2016-02-12 16:21:16 +0200

    databases class

---
+++ database/appconf.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ *Global array of configration database
+ */
+$GLOBALS['conf'] = array(
+    'host'=>'localhost',
+    'username'=>'khaled',
+    'password' =>'iti',
+    'database' =>'cafeteria'
+);
+
